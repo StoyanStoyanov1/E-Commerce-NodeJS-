df54f38ba5cb6843909f433aac0c85e8f69647c7
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-04-10 21:12:05 +0200

    Set up MongoDB integration and additional middleware
    - Added `mongoose` for database connection and configured it in `app.js`.
    - Installed and integrated `body-parser` and `morgan` as middleware for request parsing and logging.
    - Updated server configuration to fetch port and MongoDB connection string from environment variables.
    - Expanded `package.json` dependencies for new libraries.

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,25 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-
-require('dotenv/config');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const api = process.env.API_URL;
+const port = process.env.PORT || 3000;
+const connectionString = process.env.CONNECTION_STRING;
 
-app.listen(3000, () => {
-console.log(`API URL is set to: ${api}`);
-    console.log('Server is running on port 3000');
+// middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+mongoose.connect(connectionString)
+.then(() => console.log('Database connection is ready'))
+.catch(err => console.log(err));
+
+
+app.listen(port, () => {
+    console.log(`API URL is set to: ${api}`);
+    console.log(`Server is running on port ${port}`);
 });
