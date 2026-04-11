a6da4349c45309f2bdf40d243a159f5044eba86c
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-04-11 14:53:49 +0200

    Refactor app to use ES modules, add product model and router, and set up product API endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,33 @@
-require('dotenv').config();
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import ProductRouter from "./routers/product.js";
+dotenv.config();
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+import express from "express";
+
 
 const api = process.env.API_URL;
 const port = process.env.PORT || 3000;
 const connectionString = process.env.CONNECTION_STRING;
+const app = express();
 
 // middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+
+//Routers
+app.use(`${api}/products`, ProductRouter);
+
 
 mongoose.connect(connectionString)
 .then(() => console.log('Database connection is ready'))
 .catch(err => console.log(err));
 
 
-app.listen(port, () => {
+
+app.listen(port,'0.0.0.0', () => {
     console.log(`API URL is set to: ${api}`);
     console.log(`Server is running on port ${port}`);
 });
