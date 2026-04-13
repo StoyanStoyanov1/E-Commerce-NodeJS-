5223bfccaf12d0428249d191540ccedb47414237
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-04-13 11:56:26 +0200

    Add CRUD operations for categories with detailed error handling

--- backend/routers/categories.js
+++ backend/routers/categories.js
@@ -10,12 +10,27 @@ router.get("/", async (req, res) => {
         if (categoryList.length === 0) {
             return res.status(404).json({ message: "No categories found" });
         }
-        
-        res.send(categoryList);
+
+        res.status(200).send(categoryList);
     } catch (error) {
         return res.status(500).json({ success: false, error: error.message });
     }
 });
+
+router.get("/:id", async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+        const category = await Category.findById(categoryId);
+
+        if (!category) {
+            res.status(404).json({message: "No category found with this id"});
+        }
+
+        res.status(200).send(category);
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+})
 
 router.post("/", async (req, res) => {
     try {
@@ -36,6 +51,33 @@ router.post("/", async (req, res) => {
         });
     }
 });
+
+router.put("/:id", async (req, res) => {
+    try {
+        const categoryId = req.params.id;
+        const category = await Category.findByIdAndUpdate(
+            categoryId,
+            {
+                name: req.body.name,
+                icon: req.body.icon,
+                color: req.body.color,
+            },
+            { new: true }
+        );
+
+        if (!category) {
+            res.status(400).json({message: "The category cannot be updated!"});
+        }
+
+        res.status(200).send(category);
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Category could not be updated",
+            error: error.message
+        });
+    }
+})
 
 router.delete("/:id", async (req, res) => {
     try {
