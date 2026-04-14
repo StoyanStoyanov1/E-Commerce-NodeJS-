5098145de46f5c0ff74743d50ad0c9bcb80b57f3
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-04-14 17:28:14 +0200

    Add order and order item models with schema definitions

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,13 @@
 import Product from "./product.js";
 import Category from "./category.js";
 import User from "./user.js";
+import Order from "./order.js";
+import OrderItem from "./orderItem.js";
 
 export {
     Product,
     Category,
     User,
+    Order,
+    OrderItem,
 }
---
+++ backend/models/orderItem.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const OrderItemSchema = new mongoose.Schema({
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+    },
+});
+
+export default mongoose.model("OrderItem", OrderItemSchema);
