832c1501c95208cfeed95ccf535bceaabd7283ac
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-04-18 11:03:26 +0200

    Refactor authentication module with error handling and routing setup

---
+++ backend/src/prisma/client.js
@@ -0,0 +1,4 @@
+import { PrismaClient } from "prisma-client-4e03cdef83f61d72b6e28125742c468f9e59c6a43de082834e6eb94d6b85d8e4";
+const prisma = new PrismaClient();
+export default prisma;
+//# sourceMappingURL=client.js.map
